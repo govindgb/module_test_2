d9d0cd4598c8ed71c0abee4a873c670bd55b7f63
govindgb <govindbhattpnd@gmail.com>
2022-12-30 22:47:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// var fist_name = document.getElementById("fist_name").value;
+// var last_name = document.getElementById("last_name").value;
+// var email = document.getElementById("exampleInputEmail1").value;
+// var reason= document.getElementById("Reason").value;
+// console.log(fist_name)
+var flag1;
+var flag2;
+var flag3;
+var flag4;
+function check1()
+{
+   flag1 = true;
+   console.log(1)
+}
+function check2()
+{
+    flag2 = true;
+    console.log(1)
+}
+function check3()
+{
+    flag3 = true;
+    console.log(1)
+}
+function check4()
+{
+    flag4 = true;
+    console.log(1)
+}
+
+function check()
+{
+   if(flag1 && flag2 && flag3 && flag4)
+   {
+    alert("success");
+   } 
+}
